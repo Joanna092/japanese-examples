75818da438344cfc6df11e73e2cc117bb1476ae1
Joanna092 <kochanowska92@gmail.com>
2021-11-02 16:50:13 +0100

    created basic structure for header and footer

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,5 +1,6 @@
 // layout.jsx
 import React from "react";
+import "./styles.scss";
 
 const Layout = (props) => {
   return (
@@ -24,20 +25,55 @@ const Layout = (props) => {
           </div>
         </div>
       </nav>
+
+      <div className="intro bg-image shadow-2-strong">
+        <div class="mask" styleName={"background-color: rgba(0, 0, 0, 0.8);"}>
+          <div class="container d-flex align-items-center justify-content-center text-center h-100">
+            <div class="text-white">
+              <h5 class="mb-4">Search for a word in order to see translation of a word, sample sentences and add your own example</h5>
+              
+              <a
+                class="btn btn-outline-light btn-lg m-2"
+                href="https://mdbootstrap.com/docs/standard/"
+                target="_blank"
+                role="button"
+              >
+                Search
+              </a>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container py-3">{props.children}</div>
-      <footer className="p-3 bg-light">
-        <div>
-          <span className="mr-3 text-secondary">
-            Built by{" "}
+
+      <footer class="bg-dark text-center text-white">
+        <div class="container p-2 pb-0">
+          <section class="mb-2">
             <a
-              href="https://www.altcademy.com"
+              class="btn btn-outline-light btn-lg m-2"
+              role="button"
+              rel="nofollow"
               target="_blank"
-              rel="noopener noreferrer"
             >
-              Altcademy
-            </a>{" "}
-            with ☕ and 💜
-          </span>
+              About
+            </a>
+            <a
+              class="btn btn-outline-light btn-lg m-2"
+              target="_blank"
+              role="button"
+            >
+              Contact me
+            </a>
+          </section>
+        </div>
+
+        <div
+          class="text-center p-3"
+          styleName={"backgroundColor: rgba(0, 0, 0, 0.2);"}
+        >
+          <a class="text-white">Copyright@2021 Japanese Examples</a>
         </div>
       </footer>
     </React.Fragment>
