8bbce0f4e95ded7e51c4dacecf6a5f6a04197107
Joanna092 <kochanowska92@gmail.com>
2021-11-02 14:15:43 +0100

    removed index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  #  root 'static_pages#index'
-  #  get '/', to: 'static_pages#index'
 
   root to: 'static_pages#home'
   get '/', to: 'static_pages#home'
