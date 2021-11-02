62cf11173705c02cd60f18ca3e7d02f165fd157a
Joanna092 <kochanowska92@gmail.com>
2021-11-02 15:40:49 +0100

    updated navber

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -1,40 +1,47 @@
 // layout.jsx
-import React from 'react';
+import React from "react";
 
 const Layout = (props) => {
   return (
     <React.Fragment>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Japanese Examples
+          </a>
 
-    <a class="navbar-brand" href="#">Japanese Examples</a>
-    
-    <span class="navbar-text">
-      Dictionary with collaborative collection of real examples
-        </span>
-    
- 
-<div class="ms-auto">
-      <button type="button" class="btn btn-link px-3 me-2">
-          Login
-        </button>
-        <button type="button" class="btn btn-primary me-3">
-          Sign up for free
-        </button>
-   </div>
+          <span class="navbar-text">
+            Dictionary with collaborative collection of real examples
+          </span>
 
-  </div>
-</nav>
-      <div className="container py-3">
-        {props.children}
-      </div>
+          <div class="ms-auto">
+            <button type="button" class="btn btn-link px-3 me-2">
+              Login
+            </button>
+            <button type="button" class="btn btn-primary me-3">
+              Sign up for free
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div className="container py-3">{props.children}</div>
       <footer className="p-3 bg-light">
         <div>
-          <span className="mr-3 text-secondary">Built by <a href="https://www.altcademy.com" target="_blank" rel="noopener noreferrer">Altcademy</a> with ☕ and 💜</span>
+          <span className="mr-3 text-secondary">
+            Built by{" "}
+            <a
+              href="https://www.altcademy.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Altcademy
+            </a>{" "}
+            with ☕ and 💜
+          </span>
         </div>
       </footer>
     </React.Fragment>
   );
-}
+};
 
 export default Layout;
