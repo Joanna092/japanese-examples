aa310812bf23f6c1a3f57a029d7b6330f333fb8e
Joanna092 <kochanowska92@gmail.com>
2021-12-21 22:12:25 +0100

    revert environment.js, added application.css

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,23 +7,13 @@ const customConfig = {
   resolve: {
     alias: {
       '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
-      '@utils': path.resolve(__dirname, '..', '..', 'app/javascript/src/utils'),
     }
   }
 }
 
 environment.config.merge(customConfig);
 
-
-// environment.splitChunks()
+// enable split chunk with default settings with following line
+environment.splitChunks()
 
 module.exports = environment
-
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
