ed24fac2459df9c9cb3a43da2e03cf3119bcc1c5
Joanna092 <kochanowska92@gmail.com>
2021-12-21 22:09:38 +0100

    updated environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,13 +7,23 @@ const customConfig = {
   resolve: {
     alias: {
       '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
+      '@utils': path.resolve(__dirname, '..', '..', 'app/javascript/src/utils'),
     }
   }
 }
 
 environment.config.merge(customConfig);
 
-// enable split chunk with default settings with following line
-environment.splitChunks()
+
+// environment.splitChunks()
 
 module.exports = environment
+
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
