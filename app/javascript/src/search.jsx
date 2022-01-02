9b9614e93409d46a21bb6e11628311a181a1b0cb
Joanna092 <kochanowska92@gmail.com>
2022-01-02 22:16:55 +0100

    created empty search page

---
+++ app/javascript/src/search.jsx
@@ -0,0 +1,21 @@
+// home.jsx
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./layout";
+import logo from "./corona.jpg";
+
+import "./styles.scss";
+import "./search.scss";
+
+const Search = () => (
+  <Layout>
+    Search results will go here
+  </Layout>
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <Search />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
