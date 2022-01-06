66b5c3f52e4975a0a860fa4dbad386a32e0dee3b
Joanna092 <kochanowska92@gmail.com>
2022-01-06 21:26:59 +0100

    added content for sample search item

--- app/javascript/src/search.jsx
+++ app/javascript/src/search.jsx
@@ -11,7 +11,52 @@ import SmallSearch from "./small_search";
 const Search = () => (
   <Layout>
     <SmallSearch />
-    Search results will go here
+    <div class="main-container">
+      <h1>私</h1>
+      <p>わたし、watashi, me</p>
+      <p>pronoun, level 1</p>
+
+      <p>Sample sentences:</p>
+      <p>
+        騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
+      </p>
+      <p>
+        Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
+        kikoeta.
+      </p>
+      <p>I could hear someone calling my name on the noisy platform.</p>
+      <img className="thumbnail border" src={logo} alt="Logo" />
+
+      <p>Source: xxx</p>
+      <p>Added by: xxx</p>
+
+      <button
+        type="button"
+        class="btn btn-outline-dark"
+        style={{
+          display: "block",
+       
+          marginBottom: "15px",
+        }}
+      >
+        See all examples
+      </button>
+
+      <button
+        type="button"
+        class="btn btn-outline-dark"
+        style={{
+          display: "block",
+       
+          marginBottom: "15px",
+        }}
+      >
+        Add own example
+      </button>
+
+      <hr style={{width:"40%"}}></hr>
+
+    </div>
   </Layout>
 );
 
