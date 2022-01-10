f1b2747d2e9d98257ea6ab1cc5ead1e77d186ee2
Joanna092 <kochanowska92@gmail.com>
2022-01-10 22:36:32 +0100

    styled display of list of search results

--- app/javascript/src/search.jsx
+++ app/javascript/src/search.jsx
@@ -12,50 +12,131 @@ const Search = () => (
   <Layout>
     <SmallSearch />
     <div class="main-container">
-      <h1>私</h1>
-      <p>わたし、watashi, me</p>
-      <p>pronoun, level 1</p>
+      <h2>Words: 2 found</h2>
 
-      <p>Sample sentences:</p>
-      <p>
-        騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
-      </p>
-      <p>
-        Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
-        kikoeta.
-      </p>
-      <p>I could hear someone calling my name on the noisy platform.</p>
-      <img className="thumbnail border" src={logo} alt="Logo" />
+      <div class="wrapper">
+        <div class="box a">わたし、watashi, me</div>
+        <div class="box b">
+          <h1>私</h1>
+        </div>
+        <div class="box c">pronoun, level 1</div>
+      </div>
 
-      <p>Source: xxx</p>
-      <p>Added by: xxx</p>
+      <div className="sample_sentences">
+        <p>Sample sentences:</p>
+        <p>
+          騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
+        </p>
+        <p>
+          Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
+          kikoeta.
+        </p>
+        <p>I could hear someone calling my name on the noisy platform.</p>
+      </div>
 
-      <button
-        type="button"
-        class="btn btn-outline-dark"
-        style={{
-          display: "block",
-       
-          marginBottom: "15px",
-        }}
-      >
-        See all examples
-      </button>
+      <div className="row">
+        <div className="col">
+          <div class="row thumbail_box">
+            <div class="col">
+              <img className="thumbnail border" src={logo} alt="Logo" />
+            </div>
+            <div class="col thumb_auth">
+              <p>Source: xxx</p>
+              <p>Added by: xxx</p>
+            </div>
+          </div>
+        </div>
 
-      <button
-        type="button"
-        class="btn btn-outline-dark"
-        style={{
-          display: "block",
-       
-          marginBottom: "15px",
-        }}
-      >
-        Add own example
-      </button>
+        <div className="col thumb_auth">
+          <button
+            type="button"
+            class="btn btn-outline-dark inline"
+            style={{
+              display: "block",
 
-      <hr style={{width:"40%"}}></hr>
+              marginBottom: "15px",
+            }}
+          >
+            See all examples
+          </button>
 
+          <button
+            type="button"
+            class="btn btn-outline-dark inline"
+            style={{
+              display: "block",
+
+              marginBottom: "15px",
+            }}
+          >
+            Add own example
+          </button>
+        </div>
+      </div>
+
+      <hr style={{ width: "40%" }}></hr>
+
+      <div class="wrapper">
+        <div class="box a">わたし、watashi, me</div>
+        <div class="box b">
+          <h1>私</h1>
+        </div>
+        <div class="box c">pronoun, level 1</div>
+      </div>
+
+      <div className="sample_sentences">
+        <p>Sample sentences:</p>
+        <p>
+          騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
+        </p>
+        <p>
+          Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
+          kikoeta.
+        </p>
+        <p>I could hear someone calling my name on the noisy platform.</p>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div class="row thumbail_box">
+            <div class="col">
+              <img className="thumbnail border" src={logo} alt="Logo" />
+            </div>
+            <div class="col thumb_auth">
+              <p>Source: xxx</p>
+              <p>Added by: xxx</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col thumb_auth">
+          <button
+            type="button"
+            class="btn btn-outline-dark inline"
+            style={{
+              display: "block",
+
+              marginBottom: "15px",
+            }}
+          >
+            See all examples
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-outline-dark inline"
+            style={{
+              display: "block",
+
+              marginBottom: "15px",
+            }}
+          >
+            Add own example
+          </button>
+        </div>
+      </div>
+
+      <hr style={{ width: "40%" }}></hr>
     </div>
   </Layout>
 );
