6e2ac4413c6ee7dd29cab3b404518e7940926bd8
Joanna092 <kochanowska92@gmail.com>
2022-01-25 20:09:24 +0100

    finished the sketch of add new page

--- app/javascript/src/new_example.jsx
+++ app/javascript/src/new_example.jsx
@@ -2,144 +2,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Layout from "./layout";
-import logo from "./corona.jpg";
+import { useState } from "react";
 
 import "./styles.scss";
-import "./examples.scss";
-import SmallSearch from "./small_search";
+import "./new_example.scss";
 
-const NewExample = () => (
-  <Layout>
-    <SmallSearch />
-    <div class="main-container">
-      <h2 class="left">Words: 2 found</h2>
+function NewExample() {
+  const [name, setName] = useState("");
 
-      <div class="wrapper left">
-        <div class="box a">わたし、watashi, me</div>
-        <div class="box b">
-          <h1>私</h1>
-        </div>
-        <div class="box c">pronoun, level 1</div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  }
+
+  return (
+      <Layout>
+          <div className="example_form">
+          <h1>Add your own example</h1>
+    <form onSubmit={handleSubmit}>
+
+        <div className="input_field">
+      <label>Word: </label>
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        
       </div>
 
-      <div className="sample_sentences left">
-        <p>Sample sentences:</p>
-        <p>
-          騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
-        </p>
-        <p>
-          Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
-          kikoeta.
-        </p>
-        <p>I could hear someone calling my name on the noisy platform.</p>
+      <div className="input_field">
+      <label>Sentence: </label>
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+     
       </div>
 
-      <div className="row left">
-        <div className="col">
-          <div class="row thumbail_box">
-            <div class="col">
-              <img className="thumbnail border" src={logo} alt="Logo" />
-            </div>
-            <div class="col thumb_auth">
-              <p>Source: xxx</p>
-              <p>Added by: xxx</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col thumb_auth">
-          <button
-            type="button"
-            class="btn btn-outline-dark inline"
-            style={{
-              display: "block",
-
-              marginBottom: "15px",
-            }}
-          >
-            See all examples
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-outline-dark inline"
-            style={{
-              display: "block",
-
-              marginBottom: "15px",
-            }}
-          >
-            Add own example
-          </button>
-        </div>
+      <div className="input_field">
+      <label>Source: </label>
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+     
       </div>
 
-      <hr style={{ width: "40%" }}></hr>
-
-      <div class="wrapper left">
-        <div class="box a">わたし、watashi, me</div>
-        <div class="box b">
-          <h1>私</h1>
-        </div>
-        <div class="box c">pronoun, level 1</div>
+      <div className="input_field">
+      <label>Picture:</label>
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      
       </div>
 
-      <div className="sample_sentences left">
-        <p>Sample sentences:</p>
-        <p>
-          騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
-        </p>
-        <p>
-          Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
-          kikoeta.
-        </p>
-        <p>I could hear someone calling my name on the noisy platform.</p>
+      <div>
+      <input type="submit" class="btn btn-outline-dark" value="Submit" />
       </div>
 
-      <div className="row left">
-        <div className="col">
-          <div class="row thumbail_box">
-            <div class="col">
-              <img className="thumbnail border" src={logo} alt="Logo" />
-            </div>
-            <div class="col thumb_auth">
-              <p>Source: xxx</p>
-              <p>Added by: xxx</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col thumb_auth">
-          <button
-            type="button"
-            class="btn btn-outline-dark inline"
-            style={{
-              display: "block",
-
-              marginBottom: "15px",
-            }}
-          >
-            See all examples
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-outline-dark inline"
-            style={{
-              display: "block",
-
-              marginBottom: "15px",
-            }}
-          >
-            Add own example
-          </button>
-        </div>
-      </div>
-
-      <hr style={{ width: "40%" }}></hr>
+    </form>
     </div>
-  </Layout>
-);
+
+    </Layout>
+  )
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
