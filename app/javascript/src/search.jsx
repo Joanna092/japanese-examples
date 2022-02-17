08cde5cba125f548e732d409ecc401e0f9883773
Joanna092 <kochanowska92@gmail.com>
2022-02-17 20:57:30 +0100

    create search page

--- app/javascript/src/search.jsx
+++ app/javascript/src/search.jsx
@@ -9,160 +9,169 @@ import "./search.scss";
 import SmallSearch from "./small_search";
 
 class Search extends React.Component {
-
   constructor() {
-    super()
+    super();
     /*state = {
     }*/
-    this.relocateExamples = this.relocateExamples.bind(this)
-    this.relocateNew = this.relocateNew.bind(this)
+    this.relocateExamples = this.relocateExamples.bind(this);
+    this.relocateNew = this.relocateNew.bind(this);
   }
 
   relocateExamples() {
-    window.location = "/examples"
+    window.location = "/examples";
   }
 
   relocateNew() {
-    window.location = "/new_example"
+    window.location = "/new_example";
   }
 
-  render () {
+  render() {
     return (
-  <Layout>
-    <SmallSearch />
-    <div class="main-container">
-      <h2 class="left">Words: 2 found</h2>
+      <Layout>
+        <SmallSearch />
+        <div class="main-container left_side">
+          <h2 class="left">Words: 2 found</h2>
 
-      <div class="wrapper left">
-        <div class="box a">わたし、watashi, me</div>
-        <div class="box b">
-          <h1>私</h1>
-        </div>
-        <div class="box c">pronoun, level 1</div>
-      </div>
-
-      <div className="sample_sentences left">
-        <p>Sample sentences:</p>
-        <p>
-          騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
-        </p>
-        <p>
-          Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
-          kikoeta.
-        </p>
-        <p>I could hear someone calling my name on the noisy platform.</p>
-      </div>
-
-      <div className="row left">
-        <div className="col">
-          <div class="row thumbail_box">
-            <div class="col">
-              <img className="thumbnail border" src={logo} alt="Logo" />
+          <div class="wrapper left">
+            <div class="box a">わたし、watashi, me</div>
+            <div class="box b">
+              <h1>私</h1>
             </div>
-            <div class="col thumb_auth">
-              <p>Source: xxx</p>
-              <p>Added by: xxx</p>
+            <div class="box c">pronoun, level 1</div>
+          </div>
+
+          <div className="sample_sentences left">
+            <p>Sample sentence:</p>
+            <p>
+              騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
+            </p>
+            <p>
+              Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
+              kikoeta.
+            </p>
+            <p>I could hear someone calling my name on the noisy platform.</p>
+          </div>
+
+          <div className="row left">
+            <div className="col-5">
+              <div class="row thumbail_box">
+                <div class="col">
+                  <img className="thumbnail border" src={logo} alt="Logo" />
+                </div>
+                <div class="col thumb_auth">
+                  <p>Source: xxx</p>
+                  <p>Added by: xxx</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-7 thumb_auth">
+              <button
+                type="button"
+                onClick={this.relocateExamples}
+                class="search_button"
+                style={{
+                  display: "block",
+                  marginBottom: "15px",
+                }}
+              >
+                See all examples
+              </button>
+
+              <button
+                type="button"
+                onClick={this.relocateNew}
+                class="search_button"
+                style={{
+                  display: "block",
+                  marginBottom: "15px",
+                }}
+              >
+                Add own example
+              </button>
             </div>
           </div>
-        </div>
 
-        <div className="col thumb_auth">
-          <button
-            type="button"
-            onClick={this.relocateExamples}
-            class="btn btn-outline-dark inline"
+          <hr
             style={{
-              display: "block",
-              marginBottom: "15px",
+              width: "70%",
+              marginTop: "50px",
+              marginBotttom: "50px",
             }}
-          >
-            See all examples
-          </button>
+          ></hr>
 
-          <button
-            type="button"
-            onClick={this.relocateNew}
-            class="btn btn-outline-dark inline"
-            style={{
-              display: "block",
-
-              marginBottom: "15px",
-            }}
-          >
-            Add own example
-          </button>
-        </div>
-      </div>
-
-      <hr style={{ width: "40%" }}></hr>
-
-      <div class="wrapper left">
-        <div class="box a">わたし、watashi, me</div>
-        <div class="box b">
-          <h1>私</h1>
-        </div>
-        <div class="box c">pronoun, level 1</div>
-      </div>
-
-      <div className="sample_sentences left">
-        <p>Sample sentences:</p>
-        <p>
-          騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
-        </p>
-        <p>
-          Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
-          kikoeta.
-        </p>
-        <p>I could hear someone calling my name on the noisy platform.</p>
-      </div>
-
-      <div className="row left">
-        <div className="col">
-          <div class="row thumbail_box">
-            <div class="col">
-              <img className="thumbnail border" src={logo} alt="Logo" />
+          <div class="wrapper left">
+            <div class="box a">わたし、watashi, me</div>
+            <div class="box b">
+              <h1>私</h1>
             </div>
-            <div class="col thumb_auth">
-              <p>Source: xxx</p>
-              <p>Added by: xxx</p>
+            <div class="box c">pronoun, level 1</div>
+          </div>
+
+          <div className="sample_sentences left">
+            <p>Sample sentence:</p>
+            <p>
+              騒（さわ）がしいホームで誰（だれ）かが私（わたし）の名前（なまえ）を呼（よ）んでいるのが聞（き）こえた。
+            </p>
+            <p>
+              Sawagashii houmu de dare ka ga watashi no namae wo yonde iru no ga
+              kikoeta.
+            </p>
+            <p>I could hear someone calling my name on the noisy platform.</p>
+          </div>
+
+          <div className="row left">
+            <div className="col-5">
+              <div class="row thumbail_box">
+                <div class="col">
+                  <img className="thumbnail border" src={logo} alt="Logo" />
+                </div>
+                <div class="col thumb_auth">
+                  <p>Source: xxx</p>
+                  <p>Added by: xxx</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-7 thumb_auth">
+              <button
+                type="button"
+                onClick={this.relocateExamples}
+                class="search_button"
+                style={{
+                  display: "block",
+                  marginBottom: "15px",
+                }}
+              >
+                See all examples
+              </button>
+
+              <button
+                type="button"
+                class="search_button"
+                onClick={this.relocateNew}
+                style={{
+                  display: "block",
+                  marginBottom: "15px",
+                }}
+              >
+                Add own example
+              </button>
             </div>
           </div>
-        </div>
 
-        <div className="col thumb_auth">
-
-          <button
-            type="button"
-            onClick={this.relocateExamples}
-            class="btn btn-outline-dark inline"
+          <hr
             style={{
-              display: "block",
-              marginBottom: "15px",
+              width: "70%",
+              marginTop: "50px",
+              marginBotttom: "50px",
             }}
-          >
-            See all examples
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-outline-dark inline"
-            onClick={this.relocateNew}
-            style={{
-              display: "block",
-
-              marginBottom: "15px",
-            }}
-          >
-            Add own example
-          </button>
+          ></hr>
         </div>
-      </div>
-
-      <hr style={{ width: "40%" }}></hr>
-    </div>
-  </Layout>
-)}}
-
+      </Layout>
+    );
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
@@ -170,5 +179,3 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(document.createElement("div"))
   );
 });
-
-
